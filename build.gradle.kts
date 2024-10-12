6c719372b299e7f871ebc02378ded245d16a9284
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-12 22:14:10 +0100

    Removed line

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.kotlinter)
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
 }
