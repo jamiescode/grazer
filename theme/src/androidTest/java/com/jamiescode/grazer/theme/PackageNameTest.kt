58a74edc052003c61636f6499a19e5174d3674f1
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-13 17:39:03 +0100

    Added testing setup and added Timber logging

--- theme/src/androidTest/java/com/jamiescode/grazer/theme/PackageNameTest.kt
+++ theme/src/androidTest/java/com/jamiescode/grazer/theme/PackageNameTest.kt
@@ -1,4 +1,4 @@
-package com.jamiescode.grazer.login
+package com.jamiescode.grazer.theme
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -12,11 +12,11 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class PackageNameTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.jamiescode.grazer.login.test", appContext.packageName)
+        assertEquals("com.jamiescode.grazer.theme.test", appContext.packageName)
     }
 }
