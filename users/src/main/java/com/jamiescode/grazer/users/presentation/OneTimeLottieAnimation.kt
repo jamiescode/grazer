54a17278351df8f0a89726e0677f05fa05c89957
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-14 16:00:37 +0100

    Fixed imports

--- users/src/main/java/com/jamiescode/grazer/users/presentation/OneTimeLottieAnimation.kt
+++ users/src/main/java/com/jamiescode/grazer/users/presentation/OneTimeLottieAnimation.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
