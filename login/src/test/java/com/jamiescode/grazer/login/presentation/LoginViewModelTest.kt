5becab3b68c36b914ea2303f8e3d0d591c4ab73f
Jamie <6881571+jamiescode@users.noreply.github.com>
2024-10-14 20:22:47 +0100

    View model tests

--- login/src/test/java/com/jamiescode/grazer/login/presentation/LoginViewModelTest.kt
+++ login/src/test/java/com/jamiescode/grazer/login/presentation/LoginViewModelTest.kt
@@ -1,7 +1,11 @@
 package com.jamiescode.grazer.login.presentation
 
 import com.jamiescode.grazer.login.domain.usecase.LoginUseCase
+import com.jamiescode.grazer.login.presentation.LoginViewModel.State
 import com.jamiescode.grazer.navigation.AppNavigator
+import com.jamiescode.grazer.navigation.Destinations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -9,10 +13,40 @@ import org.junit.jupiter.api.Test
 class LoginViewModelTest {
     private val loginUseCase: LoginUseCase = mockk(relaxed = true)
     private val appNavigator: AppNavigator = mockk(relaxed = true)
-    val viewModel = LoginViewModel(loginUseCase, appNavigator)
+    private val viewModel = LoginViewModel(loginUseCase, appNavigator)
+    private val email = "email"
+    private val password = "password"
 
     @Test
-    fun `GIVEN X, WHEN Y, THEN Z`() {
-        assertEquals(true, false)
+    fun `GIVEN login use case success, WHEN login, THEN post success and navigate to users`() {
+        // GIVEN
+        val loginUseCaseResult = LoginUseCase.Result.Success("authToken")
+        coEvery { loginUseCase.execute(email, password) } returns loginUseCaseResult
+
+        // WHEN
+        viewModel.login(email, password)
+
+        // THEN
+        val state = viewModel.stateLiveData.value
+        assertEquals(State.Idle, state)
+
+        coVerify { loginUseCase.execute(email, password) }
+        coVerify { appNavigator.navigateTo(Destinations.Users) }
+    }
+
+    @Test
+    fun `GIVEN login use case error, WHEN login, THEN post error`() {
+        // GIVEN
+        val loginUseCaseResult = LoginUseCase.Result.Error(e = Exception("Login failed"))
+        coEvery { loginUseCase.execute(email, password) } returns loginUseCaseResult
+
+        // WHEN
+        viewModel.login(email, password)
+
+        // THEN
+        val state = viewModel.stateLiveData.value
+        assertEquals(State.Error("Credentials are incorrect"), state)
+
+        coVerify { loginUseCase.execute(email, password) }
     }
 }
--- users/src/test/java/com/jamiescode/grazer/users/presentation/UsersViewModelTest.kt
+++ users/src/test/java/com/jamiescode/grazer/users/presentation/UsersViewModelTest.kt
@@ -1,16 +1,50 @@
 package com.jamiescode.grazer.users.presentation
 
+import com.jamiescode.grazer.users.domain.User
 import com.jamiescode.grazer.users.domain.usecase.GetUsersUseCase
+import com.jamiescode.grazer.users.presentation.UsersViewModel.State
+import io.mockk.coEvery
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class UsersViewModelTest {
     private val getUsersUseCase: GetUsersUseCase = mockk(relaxed = true)
-    val viewModel = UsersViewModel(getUsersUseCase)
+    private val viewModel = UsersViewModel(getUsersUseCase)
+    private val users =
+        listOf(
+            User(
+                name = "",
+                dateOfBirthEpochSeconds = 0L,
+                profileImageUrl = "",
+            ),
+        )
 
     @Test
-    fun `GIVEN X, WHEN Y, THEN Z`() {
-        assertEquals(true, false)
+    fun `GIVEN get users use case success, WHEN loadUsers, THEN post success`() {
+        // GIVEN
+        val getUsersUseCaseResult = GetUsersUseCase.Result.Success(users)
+        coEvery { getUsersUseCase.execute() } returns getUsersUseCaseResult
+
+        // WHEN
+        viewModel.loadUsers()
+
+        // THEN
+        val state = viewModel.stateLiveData.value
+        assertEquals(State.Loaded(users), state)
+    }
+
+    @Test
+    fun `GIVEN get users use case error, WHEN loadUsers, THEN post error`() {
+        // GIVEN
+        val getUsersUseCaseResult = GetUsersUseCase.Result.Error(Exception())
+        coEvery { getUsersUseCase.execute() } returns getUsersUseCaseResult
+
+        // WHEN
+        viewModel.loadUsers()
+
+        // THEN
+        val state = viewModel.stateLiveData.value
+        assertEquals(State.Error(message = "Credentials are incorrect"), state)
     }
 }
